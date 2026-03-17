ae4f919055960ad881657310beea5e98718ea6a9
Ethan Holz <ethan.holz@thoriumworks.com>
2026-03-16 18:00:06 -0600

    feat: add lib

--- lib/macos/home.nix
+++ lib/macos/home.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: let
   inherit (pkgs) system;
+  font-size =
+    if pkgs.stdenv.isDarwin
+    then "15"
+    else "14";
 in {
   home.sessionPath = [
     "/opt/homebrew/opt/node@22/bin"
@@ -7,4 +11,28 @@ in {
     "/opt/podman/bin"
     "/Applications/Obsidian.app/Contents/MacOS"
   ];
+
+  programs.ghostty = {
+    enable = true;
+    # TODO: make this Linux capable in the future
+    package = null;
+    settings = {
+      font-size = font-size;
+      font-family = "GeistMono Nerd Font";
+      theme = "Carbonfox";
+      font-thicken = true;
+      quit-after-last-window-closed = true;
+      shell-integration-features = "no-cursor,ssh-env,ssh-terminfo";
+      #   shell-integration = fish
+      cursor-style = "block";
+      command = "${pkgs.fish}/bin/fish";
+      auto-update-channel = "tip";
+      auto-update = "download";
+      keybind = [
+        "shift+enter=text:\n"
+      ];
+    };
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+  };
 }
--- lib/shared/home.nix
+++ lib/shared/home.nix
@@ -137,30 +137,6 @@ in {
     MAMBA_ROOT_PREFIX = "~/micromamba/";
   };
 
-  programs.ghostty = {
-    enable = true;
-    # TODO: make this Linux capable in the future
-    package = null;
-    settings = {
-      font-size = font-size;
-      font-family = "GeistMono Nerd Font";
-      theme = "Carbonfox";
-      font-thicken = true;
-      quit-after-last-window-closed = true;
-      shell-integration-features = "no-cursor,ssh-env,ssh-terminfo";
-      #   shell-integration = fish
-      cursor-style = "block";
-      command = "${pkgs.fish}/bin/fish";
-      auto-update-channel = "tip";
-      auto-update = "download";
-      keybind = [
-        "shift+enter=text:\n"
-      ];
-    };
-    enableFishIntegration = true;
-    enableZshIntegration = true;
-  };
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
